52b0c0bfb178010befabc9b73bfb745ccda8fa65
Nagendra Bypina <bypina.nagendra@gmail.com>
2024-09-08 21:07:25 +0530

    getusers from random users api

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form } from "react-bootstrap";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [params, setParams] = useState({ results: "" });
+  const handleSubmit = (e) => {
+    const { results } = params;
+    console.log(results);
+    e.preventDefault();
+    axios
+      .get(`https://randomuser.me/api/?results=10`)
+      .then((users) => setUsers(users.data.results));
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    const { name, value } = e.target.value;
+    setParams({
+      ...params,
+      [name]: value,
+    });
+  };
+
+  console.log(params);
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="count">
+          <Form.Label>Results:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter results count"
+            onChange={handleChange}
+            name="results"
+          />
+        </Form.Group>
+        <ul>
+          {users.map((user, login) => {
+            return (
+              <li key={login.uuid}>
+                {user.name.first} {user.name.last} {user.phone}
+              </li>
+            );
+          })}
+        </ul>
+
+        <Button type="submit"> Get Users</Button>
+      </Form>
+    </>
+  );
+}
+
+export default App;
